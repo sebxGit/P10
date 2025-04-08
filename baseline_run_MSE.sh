23de1365174803a2278adabcd77e012af4360078
SebxGit <72699667+sebxGit@users.noreply.github.com>
2025-04-08 06:24:08 +0200

    trying without the "python" in sh

--- baseline_run_MSE.sh
+++ baseline_run_MSE.sh
@@ -8,4 +8,4 @@
 #SBATCH --cpus-per-task=20
 #SBATCH --mem=175G
 
-singularity exec --nv energycontainerblackbox.sif python3 python baselines.py --input_size 22 --seq_len 12 --batch_size 8 --criterion MSELoss --optimizer Adam  --max_epochs 1000 --n_features 7 --hidden_size 100 --num_layers 1 --dropout 0 --learning_rate 0.001 --num_workers 5 --scaler MinMaxScaler
+singularity exec --nv energycontainerblackbox.sif python3 baselines.py --input_size 22 --seq_len 12 --batch_size 8 --criterion MSELoss --optimizer Adam  --max_epochs 1000 --n_features 7 --hidden_size 100 --num_layers 1 --dropout 0 --learning_rate 0.001 --num_workers 5 --scaler MinMaxScaler
