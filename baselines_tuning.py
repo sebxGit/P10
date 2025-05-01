ba504b599fde7c84ae3aa44da4dd8fd0a21c38f4
SebxGit <72699667+sebxGit@users.noreply.github.com>
2025-05-01 10:24:28 +0200

    Update baselines_tuning.py

--- baselines_tuning.py
+++ baselines_tuning.py
@@ -541,7 +541,7 @@ def objective(args, trial):
     if isinstance(model, torch.nn.Module):
       print(f"-----Tuning {model.name} model-----")
       tuned_model = LightningModel(model=model, criterion=params['criterion'], optimizer=params['optimizer'], learning_rate=params['learning_rate'])
-      trainer = L.Trainer(max_epochs=params['max_epochs'], log_every_n_steps=0, precision='16-mixed', enable_checkpointing=False, strategy='ddp_find_unused_parameters_true')
+      trainer = L.Trainer(max_epochs=params['max_epochs'], log_every_n_steps=0, enable_checkpointing=False, strategy='ddp_find_unused_parameters_true')
       trainer.fit(tuned_model, train_dataloaders=colmod.train_dataloader())
       val_loss = trainer.callback_metrics["val_loss"].item()
 
