f3395d34baba59b6bff67dda0e0784bb0942de0e
SebxGit <72699667+sebxGit@users.noreply.github.com>
2025-05-14 06:05:42 +0200

    Update architecture_tuning.py

--- architecture_tuning.py
+++ architecture_tuning.py
@@ -374,7 +374,7 @@ class Configs:
 def create_and_save_ensemble(combined_name):
   all_predictions = []
   lengths = []
-  folder_path = f'Predictions/{combined_name}'
+  folder_path = f'Tunings/{combined_name}'
   pt_files = [f for f in os.listdir(folder_path) if f.endswith('.pt')]
   for i, pt_file in enumerate(pt_files):
     file_path = os.path.join(folder_path, pt_file)
