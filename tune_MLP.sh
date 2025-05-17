dc1ac1a52efa194cffb201726f3ae0d886c8125f
SebxGit <72699667+sebxGit@users.noreply.github.com>
2025-05-18 00:03:18 +0200

    changed to 24h

--- tune_MLP.sh
+++ tune_MLP.sh
@@ -8,4 +8,4 @@
 #SBATCH --cpus-per-task=128
 #SBATCH --mem=192G
 
-singularity exec --nv energycontainerblackbox.sif python3 baselines_tuning.py --dataset SDU --pred_len 6 --model MLP
+singularity exec --nv energycontainerblackbox.sif python3 baselines_tuning.py --dataset SDU --pred_len 24 --model MLP
