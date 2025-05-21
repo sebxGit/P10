6c269e81195a83aba3457f568be484ed267b77c2
Casper Søgård <casper-s@hotmail.dk>
2025-05-21 14:32:09 +0200

    Update baselines_tuning.py

--- baselines_tuning.py
+++ baselines_tuning.py
@@ -642,4 +642,4 @@ if __name__ == '__main__':
   parser.add_argument("--mixed", type=str, default='True')
   args = parser.parse_args()
 
-  best_params = tune_model_with_optuna(args, n_trials=3)
+  best_params = tune_model_with_optuna(args, n_trials=150)
