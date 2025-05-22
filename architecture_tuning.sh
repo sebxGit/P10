5c28939002aa8dfdb6a0f123ea5f8c6cc17673aa
SebxGit <72699667+sebxGit@users.noreply.github.com>
2025-05-22 16:43:02 +0200

    Update architecture_tuning.sh

--- architecture_tuning.sh
+++ architecture_tuning.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=p10
-#SBATCH --output=result_baselines_run_%j.out
-#SBATCH --error=error_baselines_run_%j.err
+#SBATCH --output=result_architecture_tuning_%j.out
+#SBATCH --error=error_architecture_tuning_%j.err
 #SBATCH --time=12:00:00 # 
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:8
 #SBATCH --cpus-per-task=20
-#SBATCH --mem=175G
+#SBATCH --mem=192G
 
 singularity exec --nv energycontainerblackbox.sif python3 architecture_tuning.py
