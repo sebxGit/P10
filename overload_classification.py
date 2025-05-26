fb91ab4ccb1fb261c481d291e8e071a47b840439
SebxGit <72699667+sebxGit@users.noreply.github.com>
2025-05-26 16:37:11 +0200

    Update overload_classification.py

--- overload_classification.py
+++ overload_classification.py
@@ -772,7 +772,7 @@ if __name__ == "__main__":
   if os.path.exists(file_path):
     metrics_df = pd.read_csv(file_path)
 
-  filtered_metrics_df = metrics_df[metrics_df.index.str.astype(str).contains(f"{combined_name}_part", na=False)]
+  filtered_metrics_df = metrics_df[metrics_df.index.astype(str).str.contains(f"{combined_name}_part", na=False)]
   avg_metrics_df = filtered_metrics_df[['mae', 'acc', 'pre', 'rec']].mean()
   avg_metrics_df['model'] = f"{combined_name}_avg"
   metrics_df = pd.concat([metrics_df, avg_metrics_df], ignore_index=True)
