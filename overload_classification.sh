726cd12eec075484d3caf4f150d3d52994638b9a
SebxGit <72699667+sebxGit@users.noreply.github.com>
2025-05-26 19:41:55 +0200

    Update overload_classification.sh

--- overload_classification.sh
+++ overload_classification.sh
@@ -8,4 +8,4 @@
 #SBATCH --cpus-per-task=80
 #SBATCH --mem=175G
 
-singularity exec --nv energycontainerblackbox.sif python3 overload_classification.py --dataset Colorado --threshold 500 --multiplier 2 --downscaling 13 --models GradientBoostingRegressor --individual True 
+singularity exec --nv energycontainerblackbox.sif python3 overload_classification.py --dataset Colorado --threshold 500 --multiplier 2 --downscaling 13 --models AdaBoostRegressor --individual True 
