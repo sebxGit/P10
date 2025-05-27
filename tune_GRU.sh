bfcacf50c60dec1724a8aa49a730ec03d18e5465
Casper Søgård <casper-s@hotmail.dk>
2025-05-27 19:28:09 +0200

    Update tune_GRU.sh

--- tune_GRU.sh
+++ tune_GRU.sh
@@ -8,4 +8,4 @@
 #SBATCH --cpus-per-task=20
 #SBATCH --mem=192G
 
-singularity exec --nv energycontainerblackbox.sif python3 baselines_tuning.py --dataset SDU --pred_len 24 --mixed True --model GRU --individual True
+singularity exec --nv energycontainerblackbox.sif python3 baselines_tuning.py --dataset SDU --pred_len 6 --mixed True --model GRU --individual True
