448d7ae33b9a41933973b7a19c56e1982bc6bb47
SebxGit <72699667+sebxGit@users.noreply.github.com>
2025-05-28 23:04:36 +0200

    Update baselines_tuning_classification.sh

--- baselines_tuning_classification.sh
+++ baselines_tuning_classification.sh
@@ -8,4 +8,4 @@
 #SBATCH --cpus-per-task=128
 #SBATCH --mem=192G
 
-singularity exec --nv energycontainerblackbox.sif python3 baselines_tuning_classification.py --dataset Colorado --pred_len 24  --model DPAD --individual True
+singularity exec --nv energycontainerblackbox.sif python3 baselines_tuning_classification.py --dataset Colorado --pred_len 24  --model AdaBoost --individual True
