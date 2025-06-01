c1086a70dab53c75a0ed36994f2c05b1aab63aed
SebxGit <72699667+sebxGit@users.noreply.github.com>
2025-06-01 23:31:20 +0200

    ad

---
+++ class_LSTM.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=p10
+#SBATCH --output=result_classification_tuning_%j.out
+#SBATCH --error=error_classification_tuning_%j.err
+#SBATCH --time=12:00:00 # 
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:8
+#SBATCH --cpus-per-task=128
+#SBATCH --mem=192G
+
+singularity exec --nv energycontainerblackbox.sif python3 baselines_tuning_classification.py --model LSTM --load False --individual True --dataset SDU --threshold 250
