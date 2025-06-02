9f31a55d08c568264d56e337ef85a9373fc94502
SebxGit <72699667+sebxGit@users.noreply.github.com>
2025-06-02 15:00:50 +0200

    Update baselines_tuning.py

--- baselines_tuning.py
+++ baselines_tuning.py
@@ -642,8 +642,8 @@ def objective(args, trial):
         'stride': args.pred_len,
         'batch_size': trial.suggest_int('batch_size', 32, 128, step=16) if args.model != "DPAD" else trial.suggest_int('batch_size', 16, 48, step=16),
         # 'criterion': torch.nn.L1Loss(),
-        # 'criterion': torch.nn.MSELoss(),# MSELoss is more common for regression tasks
-        'criterion': torch.nn.HuberLoss(), # Huber loss, less sensitive to outliers than MSE
+        'criterion': torch.nn.MSELoss(),# MSELoss is more common for regression tasks
+        # 'criterion': torch.nn.HuberLoss(), # Huber loss, less sensitive to outliers than MSE
         'optimizer': torch.optim.Adam,
         'scaler': MinMaxScaler(),
         'learning_rate': trial.suggest_float('learning_rate', 1e-4, 1e-2, log=True),
