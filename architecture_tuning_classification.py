c88232bf0d1c0f1038a61791f02cf4c1a9333691
SebxGit <72699667+sebxGit@users.noreply.github.com>
2025-06-02 20:23:44 +0200

    asd

--- architecture_tuning_classification.py
+++ architecture_tuning_classification.py
@@ -801,7 +801,7 @@ if __name__ == "__main__":
   for d in tuning_results:
     if d not in unique_results:
       unique_results.append(d)
-  sorted_trials = sorted(filtered_trials, key=lambda x: x.get('rec', float('inf')))
+  sorted_trials = sorted(unique_results, key=lambda x: x.get('rec', float('inf')))
   top_10_tunings = sorted_trials[:10]
   df_top_10 = pd.DataFrame(top_10_tunings)
   df_top_10.to_csv(f'Tunings/{args.dataset}_{args.pred_len}h_architecture_tuning_classification.csv', index=False)
