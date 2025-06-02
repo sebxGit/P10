ddeb6fc5d883b46d09cb3164ac05450dc2417825
Casper Søgård <casper-s@hotmail.dk>
2025-06-02 11:32:57 +0200

    Update model_run.sh

--- model_run.sh
+++ model_run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=p10
-#SBATCH --output=result_model_RandomForestRegressor_run_%j.out
-#SBATCH --error=error_model_RandomForestRegressor_run_%j.err
+#SBATCH --output=result_model_GradientBoostingRegressor_run_%j.out
+#SBATCH --error=error_model_GradientBoostingRegressor_run_%j.err
 #SBATCH --time=12:00:00 # 
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:8
 #SBATCH --cpus-per-task=128
 #SBATCH --mem=192G
 
-singularity exec --nv energycontainerblackbox.sif python3 model_run.py --model RandomForestRegressor
+singularity exec --nv energycontainerblackbox.sif python3 model_run.py --model GradientBoostingRegressor
