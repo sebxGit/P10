67781c7e9eef413301e8865657718c6863f8689c
Casper Søgård <casper-s@hotmail.dk>
2025-06-04 01:51:48 +0200

    Create EVCSForecastRun.sh

---
+++ EVCSForecastRun.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=EVCSForecastRun
+#SBATCH --output=result_EVCSForecastRun_%j.out
+#SBATCH --error=error_EVCSForecastRun_%j.err
+#SBATCH --time=12:00:00 # 
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:8
+#SBATCH --cpus-per-task=20
+#SBATCH --mem=192G
+
+
+MODELS=("LSTM", "GRU", "AdaBoost", "RandomForest", "GradientBoosting", "xPatch", "PatchMixer")
+
+for model in "${MODELS[@]}"; do
+    echo "Running model: $model"
+    singularity exec --nv energycontainerblackbox.sif \
+        python3 baselines_tuning_classification.py \
+        --model "$model" \
+        --load False \
+        --individual True \
+        --dataset SDU \
+        --threshold 250
+done
