419dfd279fc5ad47ea15841595678c166d379464
Casper Søgård <casper-s@hotmail.dk>
2025-06-04 03:13:49 +0200

    Update EVCSForecastRun.sh

--- EVCSForecastRun.sh
+++ EVCSForecastRun.sh
@@ -9,7 +9,7 @@
 #SBATCH --mem=192G
 
 
-MODELS=("LSTM", "GRU", "AdaBoostRegressor", "RandomForestRegressor", "GradientBoostingRegressor", "xPatch", "PatchMixer")
+MODELS=("LSTM", "GRU")
 
 for model in "${MODELS[@]}"; do
     echo "Running model: $model"
