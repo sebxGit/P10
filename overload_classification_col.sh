e4216eb2364f24a4a094d5001fc960a1d72036e4
Casper Søgård <casper-s@hotmail.dk>
2025-06-05 16:54:03 +0200

    Overload col

---
+++ overload_classification_col.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=p10
+#SBATCH --output=result_overload_classification_%j.out
+#SBATCH --error=error_overload_classification_%j.err
+#SBATCH --time=12:00:00 # 
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:8
+#SBATCH --cpus-per-task=80
+#SBATCH --mem=175G
+
+singularity exec --nv energycontainerblackbox.sif python3 overload_classification_col.py --dataset Colorado --threshold 500 --multiplier 2 --downscaling 13 --models "['RandomForestRegressor', 'GradientBoostingRegressor', 'AdaBoostRegressor']" --individual False
