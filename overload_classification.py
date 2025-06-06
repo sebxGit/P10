9d2e170118aba63140597bd45fd8078985163f97
Casper Søgård <casper-s@hotmail.dk>
2025-06-06 05:05:48 +0200

    Update overload_classification.py

--- overload_classification.py
+++ overload_classification.py
@@ -652,10 +652,9 @@ def initialize_model(model_name, hyperparameters):
   return model_dict[model_name]()
 
 # Gradient Boosting, LSTM, PatchMixer
-
+# Gradient Boosting, AdaBoost, GRU, PatchMixer, xPatch
 parser = ArgumentParser()
-parser.add_argument("--models", type=str,
-                    default="['GradientBoostingRegressor', 'LSTM', 'PatchMixer']")
+parser.add_argument("--models", type=str, default="['GradientBoostingRegressor', 'AdaBoostRegressor', 'GRU', 'PatchMixer', 'xPatch']")
 parser.add_argument("--individual", type=str, default="False")
 parser.add_argument("--input_size", type=int, default=16)
 parser.add_argument("--pred_len", type=int, default=24)
